907cb2f0cd71a3b77fc47fe300473e7353ef142c
김건희 <cckgh98@naver.com>
2022-10-18 14:44:59 +0900

    LeetCode 49. Group Anagrams

--- main.py
+++ main.py
@@ -9,22 +9,19 @@ import math
 import re
 
 
-def solution(paragraph, banned):
-    words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
-    count_dict = collections.defaultdict(int)
-
-    for word in words:
-        count_dict[word] += 1
-    counter = collections.Counter(count_dict)
-    result = counter.most_common()[0][0]
-
+def solution(strs: list)->list:
+    count_dict = collections.defaultdict(list)
+    for string in strs:
+        sorted_string = "".join(sorted(string))
+        count_dict[sorted_string].append(string)
+    result = sorted([value for key, value in count_dict.items()], key=lambda x:len(x))
+    print(result)
     return result
 
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
-    paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
-    banned = ["hit"]
-    solution(paragraph, banned)
+    strs = ["eat","tea","tan","ate","nat","bat"]
+    solution(strs)
 
 # https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
 
